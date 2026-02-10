19735fa15a46486d8aac1cb344361d45df4589a6
Jose Corella <jocorell@amazon.com>
2026-02-10 11:44:42 -0800

    fixes

--- doc/conf.py
+++ doc/conf.py
@@ -56,7 +56,7 @@ templates_path = ["_templates"]
 source_suffix = ".rst"  # The suffix of source filenames.
 master_doc = "index"  # The master toctree document.
 
-copyright = "%s, Amazon" % datetime.now().year  # pylint: disable=redefined-builtin
+copyright = f"{datetime.now().year}, Amazon"  # pylint: disable=redefined-builtin
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
@@ -70,7 +70,7 @@ autodoc_member_order = "bysource"
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-htmlhelp_basename = "%sdoc" % project
+htmlhelp_basename = f"{project}doc"
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {"python": ("http://docs.python.org/", None)}
--- src/not_grep/_plugin_loader.py
+++ src/not_grep/_plugin_loader.py
@@ -16,11 +16,17 @@ def _load_plugins() -> Dict[str, Callable[[str, str], bool]]:
 
     for entry_point in pkg_resources.iter_entry_points(PLUGIN_ENTRY_POINT):
         if entry_point.name in plugins:
+            existing_dist = plugins[entry_point.name].dist
+            current_dist = entry_point.dist
+
+            existing_name = existing_dist.project_name if existing_dist else "unknown"
+            current_name = current_dist.project_name if current_dist else "unknown"
+
             raise click.exceptions.UsageError(
-                f"Found conflicting entry points for '{entry_point.name}'"  # type: ignore
-                "\n Registered entry points found in projects:"
-                f"\n  {plugins[entry_point.name].dist.project_name}"
-                f"\n  {entry_point.dist.project_name}"
+                f"Found conflicting entry points for '{entry_point.name}'"
+                "Registered entry points found in projects:"
+                f"{existing_name}"
+                f"{current_name}"
             )
         plugins[entry_point.name] = entry_point
 
